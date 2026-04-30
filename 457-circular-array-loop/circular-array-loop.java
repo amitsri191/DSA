d4d606c6a8191c035041d0954b80fec66980004c
amitsri191 <amitkumarsrivastav191@gmail.com>
2026-04-30 11:40:45 +0530

    Time: 77 ms (5.12%) | Memory: 46.9 MB (7.03%) - LeetSync

---
+++ 457-circular-array-loop/circular-array-loop.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            Set<Integer> set=new HashSet<>();
+            set.add(i);
+            boolean isPos=nums[i]>0;
+            int curr=i;
+            while(true){
+                int next=nextIndx(nums,curr);
+                if(isPos){
+                    if(nums[next]<0){
+                        break;
+                    }else{
+                        if(set.contains(next)){
+                            if(curr!=next){
+                                return true;
+                            }else{
+                                break;
+                            }
+                        }
+                        set.add(next);
+                    }
+                }else{
+                    if(nums[next]>0){
+                        break;
+                    }else{
+                        if(set.contains(next)){
+                            if(curr!=next){
+                                return true;
+                            }else{
+                                break;
+                            }
+                        }
+                        set.add(next);
+                    }
+                }
+                curr=next;
+            }
+        }
+        return false;
+    }
+
+    public static int nextIndx(int[] nums,int curr){
+        int next=curr;
+        int seq=nums[curr];
+        if(seq>0){
+            int mod=seq%nums.length;
+            next= (mod+next)%nums.length;
+        }else{
+            int mod = seq % nums.length;
+            int forward = (mod + nums.length);
+            next=(forward+next)%nums.length;
+        }
+        return next;
+    }
+}
