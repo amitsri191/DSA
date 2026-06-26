38b4e9148f2fe34932048aea17f12c02fd08f9eb
amitsri191 <amitkumarsrivastav191@gmail.com>
2026-06-26 22:17:45 +0530

    Time: 19 ms (96.72%) | Memory: 65.7 MB (25.90%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -3,8 +3,13 @@ class Solution {
         Map<Integer,Integer> map= new HashMap<>();
         int sum=0,length=0;
         map.put(0,-1);
+        // for(int i:nums){
+        //     if(i==0) i=-1;
+        // }
         for(int i=0;i<nums.length;i++){
             if(nums[i]==0) nums[i]=-1;
+        }
+        for(int i=0;i<nums.length;i++){
             sum+=nums[i];
             if(map.containsKey(sum)) length=Math.max(length,i-map.get(sum));
             else map.put(sum,i);
