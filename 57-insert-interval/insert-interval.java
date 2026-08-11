20ffddbe8b11119fbec89ffada6ff5f71aaeb62d
amitsri191 <amitkumarsrivastav191@gmail.com>
2026-08-11 13:17:02 +0530

    Time: 6 ms (3.12%) | Memory: 47.2 MB (37.92%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> list=new ArrayList<>(Arrays.asList(intervals));
-        list.add(newInterval);
-        boolean mergedAny;
-
-        do{
-            mergedAny=false;
-            int n=list.size();
-            for (int i = 0; i < n; i++) {
-                for (int j = i+1; j < n; j++) {
-                    int[] curr=list.get(i);
-                    int[] next=list.get(j);
-                    if (Math.max(curr[0],next[0])<=Math.min(curr[1],next[1])){
-                        curr[0]=Math.min(curr[0],next[0]);
-                        curr[1]=Math.max(curr[1],next[1]);
-                        list.remove(j);
-                        mergedAny=true;
-                        break;
-                    }
-                }
-                if (mergedAny) break;
-                }
-            }while (mergedAny);
-
-        int[][] ans=list.toArray(new int[list.size()][]);
-        Arrays.sort(ans,Comparator.comparingInt(a->a[0]));
-        return ans;
+         list.add(newInterval);
+        list.sort(Comparator.comparingInt(a -> a[0]));
+        List<int[]> list2=new ArrayList<>();
+        int[] curr= list.get(0);
+        list2.add(curr);
+        for (int i = 1; i < list.size(); i++) {
+            int[] next=list.get(i);
+            if (curr[0]<=next[0] && curr[1]>=next[0]){
+                curr[0]=Math.min(curr[0],next[0]);
+                curr[1]=Math.max(curr[1],next[1]);
+            }else{
+                curr=next;
+                list2.add(next);
+            }
+        }
+        return list2.toArray(new int[list2.size()][]);
     }
 }
