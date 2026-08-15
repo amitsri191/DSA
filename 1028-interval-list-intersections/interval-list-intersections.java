55e395311b818c206182d59da79c929327481eca
amitsri191 <amitkumarsrivastav191@gmail.com>
2026-08-15 14:25:00 +0530

    Time: 4 ms (49.22%) | Memory: 47.4 MB (58.27%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.java
+++ 1028-interval-list-intersections/interval-list-intersections.java
@@ -1,19 +1,22 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        int[][] arr=new int[firstList.length+secondList.length+1][];
-        if (firstList.length==0 || secondList.length==0){
-            return new int[][] {};
+        if (firstList.length==0 && secondList.length==0){
+            return new int[][]{};
         }
-        int count=0;
-        for (int i = 0; i < firstList.length; i++) {
-            for (int j = 0; j < secondList.length; j++) {
-                int[] newArr=new int[2];
-                if (Math.max(firstList[i][0],secondList[j][0])<=Math.min(firstList[i][1],secondList[j][1])){
-                    newArr[0]=(Math.max(firstList[i][0],secondList[j][0]));
-                    newArr[1]=(Math.min(firstList[i][1],secondList[j][1]));
-                    arr[count++]=newArr;
+        int i=0,j=0,count=0;
+        int[][] arr=new int[firstList.length+ secondList.length+1][];
+        while (i< firstList.length && j< secondList.length){
+            int[] newArr =new int[2];
+            if (Math.max(firstList[i][0],secondList[j][0])<=Math.min(firstList[i][1],secondList[j][1])){
+                newArr[0]=(Math.max(firstList[i][0],secondList[j][0]));
+                newArr[1]=(Math.min(firstList[i][1],secondList[j][1]));
+                arr[count++]=newArr;
 
-                }
+            }
+            if (secondList[j][1]>=firstList[i][1]){
+                i++;
+            }else {
+                j++;
             }
         }
         return Arrays.copyOf(arr,count);
